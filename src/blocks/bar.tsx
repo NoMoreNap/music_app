f477d9dbd9c6af3dbc22eaf1c3dfa4c1787da872
NoMoreNap <vv2trash@gmail.com>
2023-06-14 11:01:09 +0300

    to Css module road

--- src/blocks/bar.tsx
+++ src/blocks/bar.tsx
@@ -1,41 +1,44 @@
 
 import * as React from 'react'
+import bar from './styles/bar.module.css'
+import player from './styles/player.module.css'
 
 function BarContent (): JSX.Element {
   return (
-    <div className="bar__content">
-        <div className="bar__player-progress"></div>
-                    <div className="bar__player-block">
-                        <div className="bar__player player">
-                            <div className="player__controls">
-                                <div className="player__btn-prev">
-                                    <svg className="player__btn-prev-svg">
+    <div className={bar.content}>
+        <div className={bar.progress}>
+        </div>
+            <div className={bar.block}>
+                        <div className={bar.player}>
+                            <div className={player.controls}>
+                                <div className={player.btnPrev}>
+                                    <svg className={player.prevSvg}>
                                         <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
                                     </svg>
                                 </div>
-                                <div className="player__btn-play _btn">
-                                    <svg className="player__btn-play-svg">
+                                <div className={player.btnPlay}>
+                                    <svg className={player.playSvg}>
                                         <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
                                     </svg>
                                 </div>
-                                <div className="player__btn-next">
-                                    <svg className="player__btn-next-svg">
+                                <div className={player.btnNext}>
+                                    <svg className={player.nextSvg}>
                                         <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
                                     </svg>
                                 </div>
-                                <div className="player__btn-repeat _btn-icon">
-                                    <svg className="player__btn-repeat-svg">
+                                <div className={`${player.btnRepeat} _btn-icon`}>
+                                    <svg className={player.repeatSvg}>
                                         <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
                                     </svg>
                                 </div>
-                                <div className="player__btn-shuffle _btn-icon">
-                                    <svg className="player__btn-shuffle-svg">
+                                <div className={`${player.btnShuffle} _btn-icon`}>
+                                    <svg className={player.shuffleSvg}>
                                         <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use>
                                     </svg>
                                 </div>
                             </div>
 
-                            <div className="player__track-play track-play">
+                            <div className={`${player.trackPlay} track-play`}>
                                 <div className="track-play__contain">
                                     <div className="track-play__image loading">
                                         <svg className="track-play__svg">
@@ -64,7 +67,7 @@ function BarContent (): JSX.Element {
                                 </div>
                             </div>
                         </div>
-                        <div className="bar__volume-block volume">
+                        <div className={`${bar.volume} volume`}>
                            <div className="volume__content">
                                 <div className="volume__image">
                                     <svg className="volume__svg">
@@ -76,7 +79,7 @@ function BarContent (): JSX.Element {
                                 </div>
                            </div>
                         </div>
-                    </div>
+            </div>
     </div>
   )
 }
