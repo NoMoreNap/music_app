bdb9cd5ea1ac2ed97d4c2912a37ec897f9673d67
NoMoreNap <vv2trash@gmail.com>
2023-07-01 21:08:02 +0300

    add logic of music player

--- src/blocks/bar.tsx
+++ src/blocks/bar.tsx
@@ -1,13 +1,115 @@
 
 import * as React from 'react'
+import { text } from 'stream/consumers'
 import bar from './styles/bar.module.css'
 import player from './styles/player.module.css'
+const { useRef, useState, useEffect } = React
 
-function BarContent (): JSX.Element {
+function BarContent (): React.ReactElement {
+  const track = useRef<HTMLAudioElement>(null)
+  const progressLine = useRef<HTMLLabelElement>(null)
+  const inviseLine = useRef<HTMLInputElement>(null)
+  const annotation = useRef<HTMLDivElement>(null)
+  const [isPlay, setPlay] = useState(false)
+  const [isHoverProgress, setHover] = useState('')
+  const [left, setLeft] = useState(0)
+
+  function playClickHandler (): void {
+    if (isPlay) {
+      track.current?.pause()
+      setPlay(false)
+    } else {
+      track.current?.play()
+      setPlay(true)
+    }
+  }
+  function PlayBtn (): React.ReactElement {
+    return (
+          <div onClick={playClickHandler} className={player.btnPlay}>
+              <svg className={player.playSvg}>
+                  <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
+              </svg>
+          </div>
+    )
+  }
+  function StopBtn (): React.ReactElement {
+    return (
+              <div onClick={playClickHandler} className={player.btnPlay}>
+                  <svg className={player.playSvg}>
+                      <use xlinkHref="img/icon/sprite.svg#icon-stop">
+                      </use>
+                  </svg>
+              </div>
+    )
+  }
+  function volumeChange (e: any): void {
+    if (track.current) {
+      const volume = Number(e.target.value) / 100
+      track.current.volume = volume
+    }
+  }
+
+  function progressBarClick (e: React.MouseEvent): void {
+    if (progressLine.current && inviseLine.current && track.current) {
+      const promptValue = Number(inviseLine.current.value)
+      track.current.currentTime = Number(track.current.duration) * (promptValue / 100)
+      progressLine.current.style.width = `${promptValue}%`
+    }
+  }
+  function SvgUnion (prop: { text: string }): React.ReactElement {
+    const { text } = prop
+    useEffect(() => {
+      if (annotation.current) {
+        annotation.current.style.left = `${String(left - annotation.current.offsetWidth * 0.63)}px`
+      }
+    })
+    return (
+      <div ref={annotation} className={bar.annotation}>
+        <h1 className={bar.annotationText}>{text}</h1>
+        <svg className={bar.svg}>
+            <use xlinkHref="img/icon/sprite.svg#icon-ann"></use>
+        </svg>
+  </div>
+    )
+  }
+  useEffect(() => {
+    const allTime = Number(track.current?.duration) * 1000
+    if (isPlay) {
+      window.play = setInterval(() => {
+        const curTime = Number(track.current?.currentTime) * 1000
+        if (curTime === allTime) {
+          setPlay(false)
+          clearInterval(window.play)
+          return
+        }
+        const procent = (curTime / allTime) * 100
+        if (progressLine.current) progressLine.current.style.width = `${procent}%`
+      }, 10)
+    } else {
+      clearInterval(window.play)
+    }
+  })
+  useEffect(() => {
+    if (inviseLine.current) {
+      inviseLine.current.addEventListener('mousemove', (e: any) => {
+        const valueHover = (e.offsetX / e.target.clientWidth) * parseInt(e.target.getAttribute('max'), 10)
+        const trackTime = track.current ? Math.floor(track.current.duration * (valueHover / 100)) : NaN
+        const str = `${(trackTime / 60) >> 0}:${trackTime % 60 >= 10 ? trackTime % 60 : `0${trackTime % 60}`}`
+        setHover(str)
+        setLeft(e.offsetX)
+      })
+      inviseLine.current.addEventListener('mouseout', (e: any) => {
+        setHover('')
+      })
+    }
+  })
   return (
     <div className={bar.content}>
-        <div className={bar.progress}>
-        </div>
+            {isHoverProgress ? <SvgUnion text={isHoverProgress}/> : ''}
+            <div className={bar.wrapper}>
+                <input id='progress' type='range' min='0' max='100' ref={inviseLine} onClick={progressBarClick} className={bar.progress}/>
+                <label htmlFor='progress' ref={progressLine} className={bar.active}></label>
+            </div>
             <div className={bar.block}>
                         <div className={bar.player}>
                             <div className={player.controls}>
@@ -16,11 +118,7 @@ function BarContent (): JSX.Element {
                                         <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
                                     </svg>
                                 </div>
-                                <div className={player.btnPlay}>
-                                    <svg className={player.playSvg}>
-                                        <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
-                                    </svg>
-                                </div>
+                                {isPlay ? <StopBtn /> : <PlayBtn />}
                                 <div className={player.btnNext}>
                                     <svg className={player.nextSvg}>
                                         <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
@@ -37,7 +135,6 @@ function BarContent (): JSX.Element {
                                     </svg>
                                 </div>
                             </div>
-
                             <div className={`${player.trackPlay} track-play`}>
                                 <div className="track-play__contain">
                                     <div className="track-play__image loading">
@@ -45,8 +142,9 @@ function BarContent (): JSX.Element {
                                             <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
                                         </svg>
                                     </div>
+
                                     <div className="track-play__author loading">
-                                        <a className="track-play__author-link" href="http://">Ты та..</a>
+                                        <a className="track-play__author-link" href="http://">Ты та</a>
                                     </div>
                                     <div className="track-play__album loading">
                                         <a className="track-play__album-link" href="http://">Баста</a>
@@ -75,11 +173,14 @@ function BarContent (): JSX.Element {
                                     </svg>
                                 </div>
                                 <div className="volume__progress _btn">
-                                    <input className="volume__progress-line _btn" type="range" name="range"/>
+                                    <input onInput={volumeChange} className="volume__progress-line _btn" type="range" name="range"/>
                                 </div>
                            </div>
                         </div>
             </div>
+            <figure>
+                <audio ref={track} src="music/basta.mp3"></audio>
+            </figure>
     </div>
   )
 }
